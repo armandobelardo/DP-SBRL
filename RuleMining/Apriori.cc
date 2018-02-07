30d93576a8040afe105600720efac0b604334d15
Armando Belardo <armandoubelardo@gmail.com>
2018-02-07 12:18:59 -0500

    Error with loops, but need to check in code

--- RuleMining/Apriori.cc
+++ RuleMining/Apriori.cc
@@ -7,8 +7,36 @@
 using namespace std;
 
 namespace {
-  vector<string> split(string str) {
+  string sjoin(unordered_set<string> s) {
+    stringstream ss;
+    for(int i = 0; i < s.size(); ++i) {
+      if(i != 0) {
+        ss << " ";
+      }
+      ss << s[i];
+    }
+    return ss.str();
+  }
 
+  string vjoin(vector<string> v) {
+    stringstream ss;
+    for(int i = 0; i < v.size(); ++i) {
+      if(i != 0) {
+        ss << " ";
+      }
+      ss << v[i];
+    }
+    return ss.str();
+  }
+
+  // Split string on spaces
+  vector<string> split(string str) {
+    stringstream ss(str);
+    istream_iterator<string> begin(ss);
+    istream_iterator<string> end;
+    vector<string> str_v(begin, end);
+
+    return str_v;
   }
 
   unordered_set<string> getFrequent(int num_transactions, float minsup,
@@ -23,9 +51,32 @@ namespace {
   }
 
   unordered_set<string> getLargerItemsets(unordered_set<string> frequent_items) {
-    // vector set union
+    // Turn string itemsets into their sets.
+    unordered_set<vector<string>> frequent_sets;
+    for (string itemset : frequent_items) {
+      frequent_sets.insert(split(itemset));
+    }
+    int k = frequent_sets[0].size();
 
-    return unordered_set<string>();
+    unordered_set<unordered_set<string>> larger_sets;
+    for (int i = 0; i < frequent_sets.size(); ++i) {      // Get all set unions.
+      for (int j = i+1; j < frequent_sets.size(); ++j) {
+        unordered_set<string> itemset(frequent_sets[i]);
+        for (int z = 0; z < k; ++z) {
+          itemset.insert(frequent_sets[j][z]);
+        }
+        larger_sets.insert(itemset);
+      }
+    }
+
+    unordered_set<string> true_larger_sets;
+    for (unordered_set<string> larger_set: larger_sets) {
+      if (larger_set.size() == k+1) {
+        true_larger_sets.insert(sjoin(larger_set));
+      }
+    }
+
+    return true_larger_sets;
   }
 
   unordered_set<string> largerFrequentItemsets(unordered_set<string> frequent_items,
@@ -34,7 +85,25 @@ namespace {
     unordered_map<string, int> counts;
     unordered_set<string> potential_freq = getLargerItemsets(frequent_items);
 
-    // count occurences
+    // Count occurences of potential frequent itemsets, size k+1.
+    unordered_set<vector<string>> potential_freq_s;
+    for (string itemset : potential_freq) {
+      potential_freq_s.insert(split(itemset));
+    }
+    int k = potential_freq[0].size();
+
+    bool subset = true;
+    for (int i = 0; i < potential_freq.size(); ++i) {      // FIX: FOR EACH LOOP OVER MAP
+      for (int j = 0; j < transactions.size(); ++j) {
+        for (int z = 0; z < k; ++z) {
+          if (transactions[j].find(first) == transactions[j].end()) {
+            subset = false;
+          }
+        }
+        if (subset) counts[potential_freq[i]]++;
+        subset = true;
+      }
+    }
 
     return getFrequent(transactions.size(), minsup, counts);
   }
