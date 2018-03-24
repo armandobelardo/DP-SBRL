0d251001e6867ebb7b76fcfa812b06cb047d78b9
Armando Belardo <armandoubelardo@gmail.com>
2018-03-24 12:41:31 -0400

    Added file reading code and tested it

--- BRL/data.py
+++ BRL/data.py
@@ -1,7 +1,23 @@
+#!/usr/bin/env python
+
 # Read in frequent itemsets and return as a list of lists. FIM mined using C++ code in ../RuleMining
 def readFIM(fname):
-    return []
+    return readGeneric(fname)
 
 # Read in data and return as a list of lists, one list per transaction.
 def readData(fname):
-    return []
+    return readGeneric(fname)
+
+def readGeneric(fname):
+    lol = []
+    f = open(fname, 'r')
+    for transaction in f:
+        lol.append(transaction.split())
+    f.close()
+    return lol
+
+# def main():
+#     test = readGeneric("test.txt")
+#     for list in test:
+#         print list
+# main()
