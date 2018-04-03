9c298a1d22ea4fa700a4feb0225f19386feb7a02
Armando Belardo <armandoubelardo@gmail.com>
2018-04-02 21:32:48 -0400

    laying ground work for self, guiding commenets/todos added

---
+++ RuleMining/record_sanitizer.py
@@ -0,0 +1,6 @@
+def sani_mushrooms():
+
+def sani_titanic():
+
+# Takes in the path of the data set to sanitize and the functions to sanitize with.
+def sani_generic():
--- Tests/privacy_testing.py
+++ Tests/privacy_testing.py
@@ -2,3 +2,9 @@
 import sys
 sys.path.append("../BRL")
 from mcmc import *
+
+# NEED: Create two rule lists, one private, one not. Compare scores using the score function.
+# Compare length of rule list and average length of the antecedents, compare to lambda and eta.
+# Compare the accuracy of the private Rule List on it's classification on a reserved DS, vs non-private.
+# Compare the accuracy of the private Rule List on it's classification on the non-noisey DS.
+# See the above using different epsilon values.
