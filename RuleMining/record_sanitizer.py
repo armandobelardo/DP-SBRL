29cb96d053575a101ca037c34c2c06c8eb72049b
Armando Belardo <armandoubelardo@gmail.com>
2018-04-02 23:52:46 -0400

    Added cleaning for UCI mushroom data set, from record to readable transaction

--- RuleMining/record_sanitizer.py
+++ RuleMining/record_sanitizer.py
@@ -1,6 +1,39 @@
+#!/usr/bin/env python
+import re
+
+def get_shroom_guide():
+    shroom_schema = open("../Data/Schema/shroom_schema.txt", 'r')
+    cats = []
+    shroom_guide = []
+    for att in shroom_schema:
+        att = re.split('[0-9]+. |: |=|, |,', att.strip())
+        cats.append(att[1])
+        cat_deets = {}
+        for i in range(2,len(att),2):
+            cat_deets[att[i+1]] = att[i]
+        shroom_guide.append(cat_deets)
+    shroom_schema.close()
+    return cats, shroom_guide
+
+def mushrooms(fname):
+    shroom_clean = open("../Data/shroom_clean.txt", "w+")
+    shrooms = open(fname, 'r')
+    cats, shroom_guide = get_shroom_guide()
+    transactions = []
+    for shroom in shrooms:
+        transaction = []
+        shroom_deets = shroom.strip().split(',')
+        for i in range(len(cats)):
+            if i == 0 or i == 4:
+                transaction.append(cats[i])
+            else:
+                transaction.append(cats[i]+ "=" + shroom_guide[i][shroom_deets[i]])
+        shroom_clean.write(" ".join(transaction)+"\n")
+
+    shroom_clean.close()
+    shrooms.close()
+
 def sani_mushrooms():
+    mushrooms("../Data/UCI-agaricus-lepiota.data")
 
-def sani_titanic():
-
-# Takes in the path of the data set to sanitize and the functions to sanitize with.
-def sani_generic():
+sani_mushrooms()
